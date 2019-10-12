ce92c53a765160d8564c798c155dff35a6ab8578
Chuntong Gao <heihegao@gmail.com>
2019-10-12 15:53:57 -0700

    delete unnecessary logs

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const app = express();
 const customerRoutes = require('./routes/customerRoutes');
 
-app.get('/', (req, res, next) => {
-    console.log('hey');
-});
 app.use('/customers', customerRoutes);
 
 module.exports = app;
