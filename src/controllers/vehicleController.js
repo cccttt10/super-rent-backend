c8b3f07ca26224b5e63d580eaf97942271b8652a
Chuntong Gao <heihegao@gmail.com>
2019-10-13 10:26:19 -0700

    rewrite vehicle controller with async await

--- src/controllers/vehicleController.js
+++ src/controllers/vehicleController.js
@@ -1,50 +1,35 @@
 const _db = require('../db').getDb();
-const log = require('../util/log');
 const dataHandler = require('../util/dataHandler');
 
-exports.getAllVehicles = (req, res, next) => {
-    _db.query(
-        `
-            SELECT * FROM vehicle LIMIT 1000;
-        `,
-        (err, data) => {
-            if (err) {
-                log.error(err);
-                next(err);
-            } else {
-                const totalCount = data.length;
-                if (req.query._start && req.query._end)
-                    data = dataHandler.paginate(data, req);
-                if (req.query._sort && req.query._order)
-                    data = dataHandler.sort(data, req);
-                res.status(200)
-                    .set({
-                        'X-Total-Count': totalCount,
-
-                        'Access-Control-Expose-Headers': ['X-Total-Count']
-                    })
-                    .json(data);
-            }
-        }
-    );
+exports.getAllVehicles = async (req, res, next) => {
+    let results = await _db.query(`SELECT * FROM vehicle`);
+    results = JSON.parse(JSON.stringify(results));
+    let vehicles = results[0];
+    const totalCount = vehicles.length;
+    if (req.query._start && req.query._end)
+        vehicles = dataHandler.paginate(vehicles, req);
+    if (req.query._sort && req.query._order)
+        vehicles = dataHandler.sort(vehicles, req);
+    res
+        .status(200)
+        .set({
+            'X-Total-Count': totalCount,
+            'Access-Control-Expose-Headers': [ 'X-Total-Count' ]
+        })
+        .json(vehicles);
 };
 
-exports.getVehicle = (req, res, next) => {
-    _db.query(
-        `
-            SELECT * FROM vehicle where id = "${req.params.id}";
-        `,
-        (err, data) => {
-            if (err) {
-                log.error(err);
-                next(err);
-            } else
-                res.status(200)
-                    .set({
-                        'X-Total-Count': 1,
-                        'Access-Control-Expose-Headers': ['X-Total-Count']
-                    })
-                    .json(data[0]);
-        }
+exports.getVehicle = async (req, res, next) => {
+    let results = await _db.query(
+        `SELECT * FROM vehicle where id = "${req.params.id}";`
     );
+    results = JSON.parse(JSON.stringify(results));
+    const vehicle = results[0][0];
+    res
+        .status(200)
+        .set({
+            'X-Total-Count': 1,
+            'Access-Control-Expose-Headers': [ 'X-Total-Count' ]
+        })
+        .json(vehicle);
 };
