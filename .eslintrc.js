c6b5e33ea89cd58bc6279e0885ce248ac0345208
Chuntong Gao <heihegao@gmail.com>
2019-10-13 09:27:12 -0700

    rewrite import and delete scripts with async / await using mysql2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
+// prettier-ignore
 module.exports = {
-    extends: ['eslint:recommended'],
-    env: {
-        node: true,
-        es6: true
+    "extends": ['eslint:recommended'],
+    "env": {
+        "node": true,
+        "es6": true
     },
-    overrides: [
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "overrides": [
         {
-            files: ['src/util/log.js'],
-            rules: {
+            "files": ['src/util/log.js'],
+            "rules": {
                 'no-console': 'off'
             }
         }
     ],
-    rules: {
+    "rules": {
         'no-trailing-spaces': 'error',
         'eqeqeq': 'error',
         'no-console': 'warn',
--- src/dev-data/script.js
+++ src/dev-data/script.js
@@ -1,44 +1,28 @@
 const fs = require('fs');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 const log = require('../util/log');
-const queryHandler = require('../util/queryHandler');
 
 dotenv.config({ path: './config.env' });
+let connection;
 
-const connection = mysql.createConnection({
-    host: process.env.DATABASE_IP,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    multipleStatements: true
-});
-
-const connect = () => {
-    connection.connect(err => {
-        if (err) throw err;
-        else log.success('👌 database connection successful!');
+const connect = async () => {
+    connection = await mysql.createConnection({
+        host: process.env.DATABASE_IP,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        multipleStatements: true
     });
-    connection.query(
-        `
-           USE super_rent
-        `,
-        queryHandler.defaultCallBack
-    );
-
-    connection.query(
-        `
-            SELECT DATABASE()
-        `,
-        queryHandler.defaultCallBack
-    );
+    await connection.query(`USE super_rent`);
+    await connection.query(`SELECT DATABASE()`);
+    log.success('👌 database connection successful!');
 };
 
-const importData = () => {
+const importData = async () => {
     const customers = JSON.parse(
         fs.readFileSync('src/dev-data/data/customers.json', 'utf8')
     );
-
-    connection.query(
+    await connection.query(
         `    
             CREATE TABLE customer(
             id VARCHAR(255) PRIMARY KEY,
@@ -48,10 +32,11 @@ const importData = () => {
             isClubMember BOOLEAN,
             points INT,
             fees DOUBLE)
-        `,
-        queryHandler.defaultCallBack
+        `
     );
+    log.success('👌 created customer table!');
 
+    let insertCustomersQuery = '';
     for (const customer of customers) {
         const {
             id,
@@ -62,21 +47,19 @@ const importData = () => {
             points,
             fees
         } = customer;
-        connection.query(
-            `
-                INSERT INTO customer(id, phone, name, driversLicence, isClubMember, points, fees)
-                VALUES("${id}", "${phone}", "${name}",
-                    ${driversLicence}, ${isClubMember}, ${points}, ${fees})
-            `,
-            queryHandler.defaultCallBack
-        );
+        insertCustomersQuery += `
+            INSERT INTO customer(id, phone, name, driversLicence, isClubMember, points, fees)
+            VALUES("${id}", "${phone}", "${name}",
+                ${driversLicence}, ${isClubMember}, ${points}, ${fees});
+        `;
     }
+    await connection.query(insertCustomersQuery);
+    log.success('👌 imported customers data!');
 
     const vehicles = JSON.parse(
         fs.readFileSync('src/dev-data/data/vehicles.json', 'utf8')
     );
-
-    connection.query(
+    await connection.query(
         `    
             CREATE TABLE vehicle(
             id VARCHAR(255) PRIMARY KEY,
@@ -87,10 +70,11 @@ const importData = () => {
             color VARCHAR(255),
             odometer INT,
             status ENUM("available for rent", "available for sale", "sold", "rented"))
-        `,
-        queryHandler.defaultCallBack
+        `
     );
+    log.success('👌 created vehicle table!');
 
+    let insertVehiclesQuery = '';
     for (const vehicle of vehicles) {
         const {
             id,
@@ -102,37 +86,39 @@ const importData = () => {
             odometer,
             status
         } = vehicle;
-        connection.query(
-            `
-                INSERT INTO vehicle(id, licence, make, model, year, color, odometer, status)
-                VALUES("${id}", ${licence}, "${make}", "${model}", 
-                    ${year}, "${color}", ${odometer}, "${status}")
-            `,
-            queryHandler.defaultCallBack
-        );
+        insertVehiclesQuery += `
+            INSERT INTO vehicle(id, licence, make, model, year, color, odometer, status)
+            VALUES("${id}", ${licence}, "${make}", "${model}", 
+                ${year}, "${color}", ${odometer}, "${status}");
+        `;
+    }
+
+    await connection.query(insertVehiclesQuery);
+    log.success('👌 imported vehicles data!');
+    log.success('👌 imported all data to database, done');
+    process.exit(0);
+};
+
+const deleteData = async () => {
+    await connection.query(`DROP TABLE IF EXISTS customer`);
+    await connection.query(`DROP TABLE IF EXISTS vehicle`);
+    log.success('👌 deleted all data from database, done');
+    process.exit(0);
+};
+
+const main = async () => {
+    try {
+        await connect();
+        if (process.argv[2] === '--import') {
+            await importData();
+        }
+        else if (process.argv[2] === '--delete') {
+            await deleteData();
+        }
+    } catch (err) {
+        log.error(err);
+        process.exit(1);
     }
 };
 
-const deleteData = () => {
-    connection.query(
-        `    
-            DROP TABLE IF EXISTS customer
-        `,
-        queryHandler.defaultCallBack
-    );
-
-    connection.query(
-        `    
-            DROP TABLE IF EXISTS vehicle
-        `,
-        queryHandler.defaultCallBack
-    );
-};
-
-connect();
-
-if (process.argv[2] === '--import') {
-    importData();
-} else if (process.argv[2] === '--delete') {
-    deleteData();
-}
+main();
