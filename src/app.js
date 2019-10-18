90091d8173cdec363eefc6da852f43816d2f3cb4
Chuntong Gao <heihegao@gmail.com>
2019-10-17 23:01:54 -0700

    add options handler to allow post requests from client

--- src/app.js
+++ src/app.js
@@ -3,7 +3,23 @@ const app = express();
 const cors = require('cors');
 const customerRoutes = require('./routes/customerRoutes');
 const vehicleRoutes = require('./routes/vehicleRoutes');
-
+const log = require('./util/log');
+const handleOptionsRequest = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        log.success('options');
+        const headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Credentials': false,
+            'Access-Control-Max-Age': '86400', // 24 hours
+            'Access-Control-Allow-Headers':
+                'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+        };
+        res.set(headers);
+        res.end();
+    } else next();
+};
+app.use(handleOptionsRequest);
 app.use(cors()); // Allow cross origin access
 app.use(express.json()); // Body parser
 app.use('/customers', customerRoutes);
