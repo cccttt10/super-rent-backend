1098646b2bf2aae6ca997792932721df4fcc13b5
Chuntong Gao <heihegao@gmail.com>
2019-10-20 12:25:56 -0700

    update customer controller to align with new description

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,6 +1,5 @@
 const _db = require('../db').getDb();
 const dataHandler = require('../util/dataHandler');
-const uuid = require('uuid/v4');
 
 exports.getAllCustomers = async (req, res, next) => {
     let results = await _db.query(`SELECT * FROM customers;`);
@@ -8,8 +7,7 @@ exports.getAllCustomers = async (req, res, next) => {
     let customers = results[0];
     const totalCount = customers.length;
     customers = customers.map(customer => {
-        if (customer.isClubMember === 0) customer.isClubMember = false;
-        if (customer.isClubMember === 1) customer.isClubMember = true;
+        customer.id = customer.driversLicence;
         return customer;
     });
     if (req.query._start && req.query._end)
@@ -26,12 +24,11 @@ exports.getAllCustomers = async (req, res, next) => {
 
 exports.getCustomer = async (req, res, next) => {
     let results = await _db.query(
-        `SELECT * FROM customers where id = '${req.params.id}';`
+        `SELECT * FROM customers where driversLicence = '${req.params.id}';`
     );
     results = JSON.parse(JSON.stringify(results));
     const customer = results[0][0];
-    if (customer.isClubMember === 0) customer.isClubMember = false;
-    if (customer.isClubMember === 1) customer.isClubMember = true;
+    customer.id = customer.driversLicence;
     res.status(200)
         .set({
             'X-Total-Count': 1,
@@ -41,23 +38,22 @@ exports.getCustomer = async (req, res, next) => {
 };
 
 exports.updateCustomer = async (req, res, next) => {
-    const id = req.params.id;
-    const { name, phone, driversLicence, isClubMember, points, fees } = req.body;
+    const { id, driversLicence, phone, name } = req.body;
     await _db.query(
         `
             UPDATE customers
-            SET name = '${name}',
-                phone = '${phone}',
-                driversLicence = ${driversLicence},
-                isClubMember = ${isClubMember},
-                points = ${points},
-                fees = ${fees}
-                WHERE id = '${id}';
+            SET driversLicence = "${driversLicence}", 
+                phone = "${phone}",
+                name = "${name}"
+                WHERE driversLicence = '${id}';
         `
     );
-    let results = await _db.query(`SELECT * FROM customers WHERE id = '${id}';`);
+    let results = await _db.query(
+        `SELECT * FROM customers WHERE driversLicence = '${driversLicence}';`
+    );
     results = JSON.parse(JSON.stringify(results));
     const updatedCustomer = results[0][0];
+    updatedCustomer.id = updatedCustomer.driversLicence;
     res.status(200)
         .set({
             'X-Total-Count': 1,
@@ -67,22 +63,26 @@ exports.updateCustomer = async (req, res, next) => {
 };
 
 exports.deleteCustomer = async (req, res, next) => {
-    const id = req.params.id;
-    await _db.query(`DELETE FROM customers WHERE id ='${id}'`);
+    const driversLicence = req.params.id;
+    await _db.query(
+        `DELETE FROM customers WHERE driversLicence ='${driversLicence}'`
+    );
     res.status(204).json(null);
 };
 
 exports.createCustomer = async (req, res, next) => {
-    const id = uuid();
-    const { phone, name, driversLicence, isClubMember, points, fees } = req.body;
+    const { driversLicence, phone, name } = req.body;
     let results = await _db.query(
         `
-            INSERT INTO customers(id, phone, name, driversLicence, isClubMember, points, fees)
-            VALUES("${id}", "${phone}", "${name}", ${driversLicence}, ${isClubMember}, ${points}, ${fees});
+            INSERT INTO customers(driversLicence, phone, name)
+            VALUES("${driversLicence}", "${phone}", "${name}");
         `
     );
-    results = await _db.query(`SELECT * FROM customers WHERE id = '${id}';`);
+    results = await _db.query(
+        `SELECT * FROM customers WHERE driversLicence = '${driversLicence}';`
+    );
     results = JSON.parse(JSON.stringify(results));
     const createdCustomer = results[0][0];
+    createdCustomer.id = createdCustomer.driversLicence;
     res.status(201).json(createdCustomer);
 };
