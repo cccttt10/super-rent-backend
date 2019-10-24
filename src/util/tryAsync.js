afab8b278eeb9bded56392e1458a7a882e0e5c16
Chuntong Gao <heihegao@gmail.com>
2019-10-23 22:41:36 -0700

    print unexpected error in tryAsync

--- src/util/tryAsync.js
+++ src/util/tryAsync.js
@@ -8,10 +8,13 @@ const tryAsync = asyncFn => {
         asyncFn(req, res, next).catch(err => {
             if (err instanceof SuperRentError)
                 res.status(err.statusCode).send({ message: err.message });
-            else
+            else {
+                // eslint-disable-next-line no-console
+                console.log(err);
                 res.status(500).send({
                     message: 'Your Internet connection is terrible! Please check!'
                 });
+            }
         });
 };
 
