9d080fd94f58b7e815c15c3fd96f8f76f03042c8
Chuntong Gao <heihegao@gmail.com>
2019-10-26 22:36:56 -0700

    final set up for production

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line no-console
     console.log = () => {}; // suppress all logs in production
+    require('@google-cloud/debug-agent').start();
+}
 
 const db = require('./db');
 (async () => {
     await db.initDb();
     const app = require('./app');
     const log = require('./util/log');
-    const port = process.env.PORT || 3300;
+    const port = process.env.NODE_ENV === 'development' ? 3300 : 8080;
     app.listen(port, () => {
         log.success(`App running on port ${port}...`);
     });
