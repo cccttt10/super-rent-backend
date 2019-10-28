e4ca7d17b2c59e998ca58b84397306afec93fd20
Chuntong Gao <heihegao@gmail.com>
2019-10-28 09:59:03 -0700

    use pool to achieve persistant db connection

--- src/db.js
+++ src/db.js
@@ -1,29 +1,33 @@
 const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 const log = require('./util/log');
-let _db;
+let pool;
 
-exports.initDb = async () => {
-    dotenv.config({ path: './config.env' });
-    if (_db) {
+exports.initDb = () => {
+    if (process.env.NODE_ENV === 'development')
+        dotenv.config({ path: './config.env' });
+    if (pool) {
         log.error('DB already connected! Do not reconnect!');
         throw new Error('DB already connected! Do not reconnect!');
     } else {
         // eslint-disable-next-line require-atomic-updates
-        _db = await mysql.createConnection({
+        pool = mysql.createPool({
             host: process.env.DATABASE_IP,
+            database: process.env.DATABASE_NAME,
             user: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             multipleStatements: true
         });
-        await _db.query(`USE superRentDatabase;`);
+        // await pool.query(`USE superRentDatabase;`);
         log.success('👌 DB connected');
     }
 };
 
 exports.getDb = () => {
-    if (!_db) {
+    if (!pool) {
         log.error('DB not connected! Connect first!');
         throw new Error('DB not connected! Connect first!');
-    } else return _db;
+    } else {
+        return pool;
+    }
 };
